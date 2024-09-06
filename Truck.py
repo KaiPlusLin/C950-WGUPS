1c67ff13b59757f7090a64276ece7fd192ef6cf5
Kaitlin Carleton <kcarle6@wgu.edu>
2024-09-06 15:05:03 -0700

    Create a truck object with variables

---
+++ Truck.py
@@ -0,0 +1,5 @@
+#Create a Truck object with variables
+class Truck:
+    def __init__(self, current_location, packages):
+        self.current_location = current_location
+        self.packages = packages
