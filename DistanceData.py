1b204eff3d63aba8ba97b8a3e9bb9853635d404b
Kaitlin Carleton <kcarle6@wgu.edu>
2024-09-06 17:57:31 -0700

    load address and distance data to lists

---
+++ DistanceData.py
@@ -0,0 +1,14 @@
+import csv
+
+def load_data(filename):
+    my_list = []
+    with open(filename) as distances:
+        distance_data = csv.reader(distances, delimiter=',')
+        next(distance_data)
+        for row in distance_data:
+            my_list.append(row)
+    return my_list
+
+distance_list = load_data('CSV files/distanceCSV.csv')
+for row in distance_list:
+    print(row)
