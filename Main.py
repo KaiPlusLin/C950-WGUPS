c966cd9d209735a7d40d43ff94bdb4e376b96c31
Kaitlin Carleton <kcarle6@wgu.edu>
2024-09-09 11:18:15 -0700

    fix distance_between functions

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
 # # define a function to return the distance between two addresses of the function- distance_between_addresses(address1, address2)
+from AddressData import address_list
 from DistanceData import distance_list
 
 
-def distance_between_addresses(address1, address2):
-    distance = distance_list[address1][address2]
-    print(distance)
+def distance_between(index1, index2):
+    address1 = address_list[index1]
+    address2 = address_list[index2]
+    distance = distance_list[index1][index2]
+
+    return f"{address1} {address2} {distance}"
 
 
-distance_between_addresses(6, 2)
+print(distance_between(6, 2))
