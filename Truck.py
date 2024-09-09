5dc171afcba97a730bbc1307d53495711152e6e6
Kaitlin Carleton <kcarle6@wgu.edu>
2024-09-09 11:25:08 -0700

    update truck class

--- Truck.py
+++ Truck.py
@@ -1,5 +1,11 @@
 #Create a Truck object with variables
 class Truck:
-    def __init__(self, current_location, packages):
+    def __init__(self, driver, mileage, speed, current_time, route, current_location, max_packages, current_packages):
+        self.driver = driver
+        self.mileage = mileage
+        self.speed = speed
+        self.current_time = current_time
+        self.route = route
         self.current_location = current_location
-        self.packages = packages
+        self.max_packages = max_packages
+        self.current_packages = current_packages
